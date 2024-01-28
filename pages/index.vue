51e9f02eccd1963d9d95605156215b715de00b12
Maciej Toborek <maciej.toborek@so.energy>
2024-01-28 19:45:40 +0000

    simplyfy auth

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
   <UContainer>
-    <h1 class="text-xl">Welcome to the home page</h1>
+    <h1 class="text-xl">Welcome to the home page {{ userName }}</h1>
   </UContainer>
 </template>
+<script setup lang="ts">
+  import { useAuthStore } from '@/store/auth'
+
+  let userName = useAuthStore().userName
+
+  definePageMeta({
+    middleware: ['auth'],
+    // or middleware: 'auth'
+  })
+</script>
--- store/auth.js
+++ store/auth.js
@@ -1,35 +1,24 @@
+const supabase = useSupabaseClient()
+
 export const useAuthStore = defineStore('auth', {
   state: () => ({ user: null, session: null }),
   getters: {
     isLoggedIn() {
       return !!this.user
     },
+    userName() {
+      return this.user?.user_metadata.full_name
+    },
   },
   actions: {
     async signIn() {
-      const supabase = useSupabaseClient()
-
-      const { error, user } = supabase.auth.signInWithOAuth({
+      const res = await supabase.auth.signInWithOAuth({
         provider: 'google',
       })
-
-      if (error) {
-        console.log(error)
-        return
-      }
-
-      this.user = user
     },
 
     async signOut() {
-      const supabase = useSupabaseClient()
       const { error } = await supabase.auth.signOut()
-
-      if (error) {
-        console.log(error)
-        return
-      }
-
       window.location.reload()
     },
 
